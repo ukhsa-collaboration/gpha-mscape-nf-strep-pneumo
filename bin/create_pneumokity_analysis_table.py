ff1d661c6b976377e5590a0e34e04ce548bee2a7
Deborah Greer <deborah.greer@ukhsa.gov.uk>
2026-02-20 17:39:38 +0000

    chore: Update mdocstring in create_pneumokity_analysis_table.py

--- bin/create_pneumokity_analysis_table.py
+++ bin/create_pneumokity_analysis_table.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-"""Module containing functions required to add strep pneumo serotyping
-results to an onyx analysis table.
+"""Script and associated functions to take strep pneumo serotyping
+results and create an onyx analysis table format json file.
 """
 
 # Imports
